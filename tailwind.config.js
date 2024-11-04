f24bb4ad643aa107b0a5abd33f99fc6c9149183b
Magnus Kutuson <magnus.kutuson@amalitech.com>
2024-11-04 17:26:19 +0000

    finished with header of the join organisation page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    colors:{
+      'deep-purple':'#65107F',
+      'light-purple':'#FCF3FF'
+    },
+   
+   extend: {},
   },
   plugins: [],
 }
