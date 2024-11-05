5ba0319aa30c241cb6383f1c574e2cdcb5cf6ca5
Abdel <abdulrashidissah232@gmail.com>
2024-11-05 08:41:02 +0000

    finished the login design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginLeftComponentComponent } from "./shared/login-left-component/login-left-component.component";
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginLeftComponentComponent],
+  imports: [RouterOutlet, LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { LoginLeftComponentComponent } from '../shared/login-left-component/login-left-component.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [LoginLeftComponentComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+
+  constructor() { }
+
+imgSrc: string ='../../assets/icons/showPassword.svg'
+  
+togglePasswordVisibility() {
+      const passwordInput = document.getElementById('password') as HTMLInputElement;
+
+      if (passwordInput.type === 'password') {
+          passwordInput.type = 'text';
+          this.imgSrc = '../../assets/icons/hide-password.svg';
+      } else {
+          passwordInput.type = 'password';
+          this.imgSrc = '../../assets/icons/showPassword.svg';
+         
+      }
+  }
 
 }
